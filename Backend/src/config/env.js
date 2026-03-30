e40d2bd17ab934e8a30930588313001b8d26d680
benjaalasino <balasino617@alumnos.iua.edu.ar>
2026-03-30 20:15:53 -0300

    .

--- Backend/src/config/env.js
+++ Backend/src/config/env.js
@@ -8,6 +8,6 @@ module.exports = {
     databaseUrl: process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5432/cuentas_claras',
     azureDocumentIntelligenceEndpoint: process.env.AZURE_DOCUMENT_INTELLIGENCE_ENDPOINT,
     azureDocumentIntelligenceKey: process.env.AZURE_DOCUMENT_INTELLIGENCE_KEY,
-    n8nWebhookUrl: process.env.N8N_WEBHOOK_URL || 'https://primary-production-1bf44.up.railway.app/webhook/1792b800-adc9-4d38-a7cc-f69017532c19',
+    n8nWebhookUrl: process.env.N8N_WEBHOOK_URL || 'https://primary-production-1bf44.up.railway.app/webhook-test/1792b800-adc9-4d38-a7cc-f69017532c19',
     appUrl: process.env.APP_URL || 'http://localhost:3000'
 };
